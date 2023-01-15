480a4ab9c78a257ee08741ee7251c4564cc647dd
Manny-Manuel <mannymanueldev@gmail.com>
2023-01-15 22:22:47 +0100

    Create project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Terminal random color generator.js
+
+## Usage
+
+When a user enter node.js in the command line a block of approximately 31x9 # characters with random color will be generated (using hex code,
+e.g. #ff0000)
+
+```bash
+$ node index.js
+##########################################################################################################################################################################################################################################
+```
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('You are a machine');
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+//User prompt
+const hue = process.argv[2];
+const luminosity = process.argv[3];
+
+const randColorRgb = Math.floor(Math.random() * 16777215).toString(16);
+const color = randomColor({
+  luminosity: luminosity,
+  hue: hue,
+});
+console.log(chalk.hex(randColorRgb).bold('###############################'));
+console.log(chalk.hex(randColorRgb).bold('###############################'));
+console.log(chalk.hex(randColorRgb).bold('###############################'));
+console.log(chalk.hex(randColorRgb).bold(`#####                     #####`));
+console.log(chalk.hex(randColorRgb).bold(`#####       ${color}       #####`));
+console.log(chalk.hex(randColorRgb).bold('#####                     #####'));
+console.log(chalk.hex(randColorRgb).bold('###############################'));
+console.log(chalk.hex(randColorRgb).bold('###############################'));
+console.log(chalk.hex(randColorRgb).bold('###############################'));
